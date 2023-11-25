fd9194ae030c7a6abe859c2c92fa8f36232f7052
Jakub Tesař <73097855+JakubTesar@users.noreply.github.com>
2023-11-25 11:01:40 +0100

    Start

---
+++ Hamming-code/src/main/java/com/example/hammingcode/HammingCodeApplication.java
@@ -0,0 +1,13 @@
+package com.example.hammingcode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HammingCodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HammingCodeApplication.class, args);
+    }
+
+}
---
+++ Hamming-code/src/test/java/com/example/hammingcode/HammingCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hammingcode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HammingCodeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
